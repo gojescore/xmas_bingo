3599152c98405d157fadc68213a1fc70c9642e6d
gojescore <apbeyer@hotmail.com>
2025-11-22 18:32:36 +0100

    Add files via upload

---
+++ public/data/deck/julekortet.js
@@ -0,0 +1,14 @@
+export const juleKortetDeck = [
+  {
+    id: "j1",
+    type: "JuleKortet",
+    title: "JuleKortet #1 – Tak til nissen",
+    text: "Skriv et julekort til nissen: Hvad takker I for i år?",
+  },
+  {
+    id: "j2",
+    type: "JuleKortet",
+    title: "JuleKortet #2 – Sjov klassejul",
+    text: "Skriv et kort til en anden klasse: ønsk dem sjov jul!",
+  },
+];
