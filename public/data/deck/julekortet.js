8ab78163080d32ba8cf8042377fa38d8a9de8115
gojescore <apbeyer@hotmail.com>
2025-11-23 10:46:08 +0100

    Update julekortet.js

--- public/data/deck/julekortet.js
+++ public/data/deck/julekortet.js
@@ -1,18 +1,14 @@
-// public/data/deck/julekortet.js
-
-export const DECK = [
+export const juleKortetDeck = [
   {
-    id: "jk1",
+    id: "j1",
     type: "JuleKortet",
     title: "JuleKortet #1 – Tak til nissen",
     text: "Skriv et julekort til nissen: Hvad takker I for i år?",
-    used: false
   },
   {
-    id: "jk2",
+    id: "j2",
     type: "JuleKortet",
     title: "JuleKortet #2 – Sjov klassejul",
     text: "Skriv et kort til en anden klasse: ønsk dem sjov jul!",
-    used: false
-  }
+  },
 ];
