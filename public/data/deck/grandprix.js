78245d4c9475731b099948daf06d6deab1266edf
gojescore <apbeyer@hotmail.com>
2025-11-23 10:46:28 +0100

    Update grandprix.js

--- public/data/deck/grandprix.js
+++ public/data/deck/grandprix.js
@@ -1,11 +1,11 @@
 // public/data/deck/grandprix.js
 
-export const DECK = [
+export const grandprixDeck = [
   {
     id: "gp1",
     type: "Nisse Grandprix",
     title: "Grandprix 1",
-    audioUrl: "PASTE_SUPABASE_URL_1",
+    audioUrl: "https://ldaskskrbotxxhoqdzdc.supabase.co/storage/v1/object/public/grandprix-audio/hojtFraT.mp3",
     used: false
   },
   {
@@ -15,4 +15,5 @@ export const DECK = [
     audioUrl: "https://ldaskskrbotxxhoqdzdc.supabase.co/storage/v1/object/public/grandprix-audio/SorenBanjo.mp3",
     used: false
   }
+
 ];
