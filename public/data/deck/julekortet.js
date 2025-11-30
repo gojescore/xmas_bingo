ddf9fc5b852af59e124c9cb5ad9a7dd50eee04ec
gojescore <apbeyer@hotmail.com>
2025-11-30 09:01:52 +0100

    Update julekortet.js

--- public/data/deck/julekortet.js
+++ public/data/deck/julekortet.js
@@ -14,34 +14,35 @@ export const juleKortetDeck = [
     answer: "Eleverne stemmer",
   },
     {
-    id: "j2",
+    id: "j3",
     type: "JuleKortet",
     title: "Uartiglisten",
     text: "Den hurtigste måde at komme på uartig-listen!",
     answer: "Eleverne stemmer",
   },
       {
-    id: "j2",
+    id: "j4",
     type: "JuleKortet",
     title: "Mellemnavn",
     text: "Hvad er julemanden hemmelige mellemnavn?",
     answer: "Eleverne stemmer",
   },
         {
-    id: "j2",
+    id: "j5",
     type: "JuleKortet",
     title: "julegaven",
     text: "En lyd, man ikke har lyst til at høre, når man ryster sin julegave!",
     answer: "Eleverne stemmer",
   },
         {
-    id: "j2",
+    id: "j6",
     type: "JuleKortet",
     title: "Julehilsen",
     text: Skriv den varmeste julehilsen til alle på skolen.",
     answer: "Eleverne stemmer",
   },
 ];
+
 
 
 
