9895872a1cf79b9046c35307d7e546e40965a1a0
gojescore <apbeyer@hotmail.com>
2025-12-04 11:41:11 +0100

    Update kreanissen.js

--- public/data/deck/kreanissen.js
+++ public/data/deck/kreanissen.js
@@ -7,7 +7,7 @@ export const DECK = [
     type: "KreaNissen",
     title: "KreaNissen 1",
     text: "Lav den sjoveste nisse med ting i klassen. Tag et billede!",
-    answer: "Eleverne stemmer",
+    answer: "Eleverne stemmer (sjoveste nisse)",
     used: false
   },
   {
@@ -15,16 +15,17 @@ export const DECK = [
     type: "KreaNissen",
     title: "KreaNissen 2",
     text: "Byg en mini-juleby på bordet. Tag et billede!",
-    answer: "Eleverne stemmer",
+    answer: "Eleverne stemmer (juleby)",
     used: false
   },
   {
     id: "kn3",
     type: "KreaNissen",
     title: "KreaNissen 3",
-    text: "Lav en kreativ julehat eller julefigur. Tag et billede!",
-    answer: "Eleverne stemmer",
+    text: "Lav en kreativ julehat. Tag et billede!",
+    answer: "Eleverne stemmer (julehat)",
     used: false
   }
 ];
+
 
