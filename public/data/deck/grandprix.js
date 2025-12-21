a7b1426c3850065b8f37da31723ca3fbaa83dbe4
gojescore <apbeyer@hotmail.com>
2025-12-21 10:12:12 +0100

    Update grandprix.js

--- public/data/deck/grandprix.js
+++ public/data/deck/grandprix.js
@@ -4,7 +4,7 @@ export const grandprixDeck = [
   {
     id: "gp1",
     type: "Nisse Grandprix",
-    title: "Grandprix 1",
+    title: "GP - Højt fra træets grønne top",
     text: "Lyt til lydklippet, vær først til at trykke BUZZ, og skriv titlen.", // 👈 opgave-tekst
     audioUrl: "https://ldaskskrbotxxhoqdzdc.supabase.co/storage/v1/object/public/grandprix-audio/hojtFraT.mp3",
     answer: "Højt fra træets grønne top",
@@ -13,7 +13,7 @@ export const grandprixDeck = [
   {
     id: "gp2",
     type: "Nisse Grandprix",
-    title: "Grandprix 2",
+    title: "GP - Søren Banjomus",
     text: "Lyt til lydklippet, vær først til at trykke BUZZ, og skriv titlen.", // 👈 opgave-tekst
     audioUrl: "https://ldaskskrbotxxhoqdzdc.supabase.co/storage/v1/object/public/grandprix-audio/SorenBanjo.mp3",
     answer: "Søren Banjomus",
@@ -22,7 +22,7 @@ export const grandprixDeck = [
   {
     id: "gp3",
     type: "Nisse Grandprix",
-    title: "Grandprix 3",
+    title: "GP - På loftet sidder nissen",
     text: "Lyt til lydklippet, vær først til at trykke BUZZ, og skriv titlen.", // 👈 opgave-tekst
     audioUrl: "https://ldaskskrbotxxhoqdzdc.supabase.co/storage/v1/object/public/grandprix-audio/Paa%20Loftet%20Sidder%20Nissen%20Med%20Sin%20Julegroed.mp3",
     answer: "På loftet sidder nissen",
@@ -31,7 +31,7 @@ export const grandprixDeck = [
     {
     id: "gp4",
     type: "Nisse Grandprix",
-    title: "Grandprix 4",
+    title: "GP - Tinka",
     text: "Lyt til lydklippet, vær først til at trykke BUZZ, og skriv titlen.", // 👈 opgave-tekst
     audioUrl: "https://ldaskskrbotxxhoqdzdc.supabase.co/storage/v1/object/public/grandprix-audio/EdTinkas%20Juleeventyrs.mp3",
     answer: "Tinka",
@@ -39,6 +39,7 @@ export const grandprixDeck = [
   }
   
 ];
+
 
 
 
